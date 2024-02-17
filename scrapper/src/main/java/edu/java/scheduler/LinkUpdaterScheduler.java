76ee7d3067c888a98ede0bfdcd8f23e1edb99196
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-02-17 16:21:54 +0300

    feat: add scheduling

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -2,10 +2,13 @@ package edu.java.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class LinkUpdaterScheduler {
-    @Scheduled(fixedDelayString = "#{ ${app.scheduler.interval} }")
+    // TODO: написать получше (использовать короткое имя bean'а)
+    @Scheduled(fixedDelayString = "#{@'app-edu.java.configuration.ApplicationConfig'.scheduler().interval()}")
     public void update() {
         log.info("Updated!");
     }
