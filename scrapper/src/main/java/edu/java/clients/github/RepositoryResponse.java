cf9c88e74190bd8ca2a9c7543a50dfa287ecdcc8
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-02-17 12:12:28 +0300

    feat: use records to store responses

--- scrapper/src/main/java/edu/java/clients/github/RepositoryResponse.java
+++ scrapper/src/main/java/edu/java/clients/github/RepositoryResponse.java
@@ -2,18 +2,12 @@ package edu.java.clients.github;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 import java.time.OffsetDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@ToString
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class RepositoryResponse {
+public record RepositoryResponse(
+    Long id,
     @JsonProperty("updated_at")
-    private OffsetDateTime updatedAt;
+    OffsetDateTime updatedAt
+) {
 }
