7d706d7ecbbffd08cea5a17b348c34db92be9d96
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-02-17 12:17:39 +0300

    feat: add rest client bean creation

---
+++ scrapper/src/main/java/edu/java/clients/ClientConfiguration.java
@@ -0,0 +1,22 @@
+package edu.java.clients;
+
+import edu.java.clients.github.GitHubClient;
+import edu.java.clients.github.GitHubWebClient;
+import edu.java.clients.stackoverflow.StackOverflowClient;
+import edu.java.clients.stackoverflow.StackOverflowWebClient;
+import edu.java.configuration.ApplicationConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientConfiguration {
+    @Bean
+    public StackOverflowClient stackOverflowClient(ApplicationConfig applicationConfig) {
+        return new StackOverflowWebClient(applicationConfig.client().stackOverflow().baseUrl());
+    }
+
+    @Bean
+    public GitHubClient gitHubClient(ApplicationConfig applicationConfig) {
+        return new GitHubWebClient(applicationConfig.client().gitHub().baseUrl());
+    }
+}
--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -9,8 +9,18 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotNull
-    Scheduler scheduler
+    Scheduler scheduler,
+    @NotNull
+    Client client
 ) {
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+    }
+
+    public record Client(GitHub gitHub, StackOverflow stackOverflow) {
+        public record GitHub(String baseUrl) {
+        }
+
+        public record StackOverflow(String baseUrl) {
+        }
     }
 }
