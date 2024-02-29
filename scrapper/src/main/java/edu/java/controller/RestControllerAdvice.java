dd9d2fc833e156c6796c9c162c2f1be7a1dcc452
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-03-01 01:39:12 +0300

    feat: update exception handling

--- scrapper/src/main/java/edu/java/controller/RestControllerAdvice.java
+++ scrapper/src/main/java/edu/java/controller/RestControllerAdvice.java
@@ -1,5 +1,8 @@
 package edu.java.controller;
 
+import edu.java.exceptions.ChatAlreadyRegisteredException;
+import edu.java.exceptions.LinkAlreadyExistsException;
+import edu.java.exceptions.NoSuchChatException;
 import edu.java.model.exception.ApiErrorResponse;
 import java.util.Arrays;
 import org.springframework.http.HttpStatus;
@@ -10,7 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiErrorResponse> handleException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiErrorResponse> notValidMethodArgument(MethodArgumentNotValidException exception) {
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(ApiErrorResponse.builder()
@@ -18,6 +21,45 @@ public class RestControllerAdvice {
                 .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                 .exceptionName(exception.getClass().getSimpleName())
                 .exceptionMessage(exception.getDetailMessageCode())
+                .stacktrace(Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList())
+                .build());
+    }
+
+    @ExceptionHandler(ChatAlreadyRegisteredException.class)
+    public ResponseEntity<ApiErrorResponse> chatAlreadyExists(ChatAlreadyRegisteredException exception) {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiErrorResponse.builder()
+                .description("Chat already exists")
+                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .exceptionName(exception.getClass().getSimpleName())
+                .exceptionMessage(exception.getMessage())
+                .stacktrace(Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList())
+                .build());
+    }
+
+    @ExceptionHandler(LinkAlreadyExistsException.class)
+    public ResponseEntity<ApiErrorResponse> linkAlreadyExists(LinkAlreadyExistsException exception) {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiErrorResponse.builder()
+                .description("Link already exists")
+                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .exceptionName(exception.getClass().getSimpleName())
+                .exceptionMessage(exception.getMessage())
+                .stacktrace(Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList())
+                .build());
+    }
+
+    @ExceptionHandler(NoSuchChatException.class)
+    public ResponseEntity<ApiErrorResponse> noSuchChatException(NoSuchChatException exception) {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiErrorResponse.builder()
+                .description("No such chat exception")
+                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .exceptionName(exception.getClass().getSimpleName())
+                .exceptionMessage(exception.getMessage())
                 .stacktrace(Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList())
                 .build());
     }
