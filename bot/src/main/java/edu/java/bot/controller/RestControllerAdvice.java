1aab6bb6aa9821e8da1a7bf55ca70a025c00962e
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-03-01 01:39:12 +0300

    feat: update exception handling

--- bot/src/main/java/edu/java/bot/controller/RestControllerAdvice.java
+++ bot/src/main/java/edu/java/bot/controller/RestControllerAdvice.java
@@ -1,5 +1,6 @@
 package edu.java.bot.controller;
 
+import edu.java.bot.exceptions.NoSuchChatException;
 import edu.java.bot.model.exceptions.ApiErrorResponse;
 import java.util.Arrays;
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiErrorResponse> handleException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiErrorResponse> nonValidMethodArgumentException(MethodArgumentNotValidException exception) {
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(ApiErrorResponse.builder()
-                .description("Некорректные параметры запроса")
+                .description("Invalid request param")
                 .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                 .exceptionName(exception.getClass().getSimpleName())
                 .exceptionMessage(exception.getDetailMessageCode())
+                .stacktrace(Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList())
+                .build());
+    }
+
+    @ExceptionHandler(NoSuchChatException.class)
+    public ResponseEntity<ApiErrorResponse> noSuchChatException(NoSuchChatException exception) {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiErrorResponse.builder()
+                .description("Chat not found")
+                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .exceptionName(exception.getClass().getSimpleName())
+                .exceptionMessage(exception.getMessage())
                 .stacktrace(Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList())
                 .build());
     }
