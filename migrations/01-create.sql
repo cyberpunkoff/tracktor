79a12bac419d8643e5c3474be6a2db7c640e0624
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-03-14 01:52:26 +0300

    feat: update migration

--- migrations/01-create.sql
+++ migrations/01-create.sql
@@ -3,22 +3,22 @@
 --changeset vasilii:1
 CREATE TABLE chats
 (
-    id      bigint generated always as identity unique,
-    chat_id bigint not null
+    chat_id bigint not null unique
 );
 
 --changeset vasilii:2
 CREATE TABLE links
 (
     id  bigint generated always as identity unique,
-    url text not null,
-    updated_at timestamp with time zone
+    url text not null unique,
+    checked_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp with time zone NOT NULL DEFAULT NOW()
 );
 
 --changeset vasilii:3
 CREATE TABLE chats_links
 (
-    chat_id bigint REFERENCES chats (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    chat_id bigint REFERENCES chats (chat_id) ON UPDATE CASCADE ON DELETE CASCADE,
     link_id bigint REFERENCES links (id) ON UPDATE CASCADE,
     CONSTRAINT chats_links_pk PRIMARY KEY (chat_id, link_id)
 );
