6bc9c57dd04240920c473927e39a5494ca119b8a
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-03-31 01:42:22 +0300

    feat: add rate limiting to bot

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -4,8 +4,10 @@ import edu.java.bot.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
     public static void main(String[] args) {
