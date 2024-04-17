2791a47767df58b777175d7b3b6142c357982b8c
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-04-17 17:38:42 +0300

    fix: resolve checkstyle issues

--- scrapper/src/main/java/edu/java/service/updater/LinkUpdater.java
+++ scrapper/src/main/java/edu/java/service/updater/LinkUpdater.java
@@ -1,12 +1,13 @@
 package edu.java.service.updater;
 
 import edu.java.LinkUpdateRequest;
-import edu.link.links.Link;
 import java.time.OffsetDateTime;
 import java.util.List;
 
 public abstract class LinkUpdater {
     public abstract List<LinkUpdateRequest> getLinkUpdates();
+
     public abstract boolean isLinkUpdated();
+
     public abstract OffsetDateTime getUpdatedAt();
 }
